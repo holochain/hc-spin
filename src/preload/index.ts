eeee6bd27c3abd6977565200fe338535b8a6fa4f
Matthias <tuttiacc@hotmail.com>
2024-01-25 14:52:47 +0100

    added support for decoupled zome call signer

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -2,7 +2,14 @@
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
 import { contextBridge, ipcRenderer } from 'electron';
 import { ZomeCallUnsignedNapi } from '@holochain/hc-spin-rust-utils';
+import { CallZomeRequestUnsigned } from '@holochain/client';
+
+contextBridge.exposeInMainWorld('__HC_ZOME_CALL_SIGNER__', {
+  signZomeCall: (zomeCall: CallZomeRequestUnsigned) =>
+    ipcRenderer.invoke('sign-zome-call', zomeCall),
+});
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  signZomeCall: (zomeCall: ZomeCallUnsignedNapi) => ipcRenderer.invoke('sign-zome-call', zomeCall),
+  signZomeCall: (zomeCall: ZomeCallUnsignedNapi) =>
+    ipcRenderer.invoke('sign-zome-call-legacy', zomeCall),
 });
